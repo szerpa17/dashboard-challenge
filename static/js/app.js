0a391e48102c6d91bb07582b66a4d87c242d21eb
szerp <szerp002@gmail.com>
2020-09-20 21:28:52 -0400

    updated test subject ID drop down code

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,20 @@
+// Select all data
+var fullDataset = d3.json('samples.json');
+
+// Populate 'Test Subject ID No' drop down
+var sel = d3.select("#selDataset");
+console.log(sel);
+
 d3.json('samples.json').then(data => {
-    console.log(data);
-    
-    
+    console.log(data);    
+
+    data.names.forEach(name => {
+        sel
+        .append("option")
+        .text(name)
+        .property("value", name);
+
+    });
 
 })
+
