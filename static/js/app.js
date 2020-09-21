7bbd70e6c63b4bd31ca866cb2d2cf1637cb1b01e
szerp <szerp002@gmail.com>
2020-09-20 21:50:07 -0400

    added function to populate metadata and a listener for the filter change

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,11 @@
 // Select all data
 var fullDataset = d3.json('samples.json');
 
-// Populate 'Test Subject ID No' drop down
+// Select drop down object
 var sel = d3.select("#selDataset");
-console.log(sel);
 
+
+// Populate 'Test Subject ID No' drop down
 d3.json('samples.json').then(data => {
     console.log(data);    
 
@@ -18,3 +19,25 @@ d3.json('samples.json').then(data => {
 
 })
 
+// Populate metadata
+function populateMetadata(selectedSampleID){
+        fullDataset.then(data => {
+        
+        // Select metadata from complete dataset
+        var metadata = data.metadata;
+        
+        // Filter metadata by sample ID (selectedSample variable) 
+        var sampleData = metadata.filter(sample => sample.id == selectedSampleID);
+        var result = sampleData[0];
+        var mdPanel = d3.select("#sample-metadata");
+        result.forEach(row => {
+            mdPanel
+            .append("br")
+            .text(row)
+            .property("value", row);
+        });
+    });
+}
+
+// Listener
+sel.on('change', populateMetadata(sel.value));
