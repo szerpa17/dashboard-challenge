d5598a59fca1ecaaa91dffe5925766d4fd111f28
szerp <szerp002@gmail.com>
2020-09-21 20:35:24 -0400

    updated coments and sort function

--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,7 @@ var sel = d3.select("#selDataset");
 
 
 // Populate 'Test Subject ID No' drop down
-d3.json('samples.json').then(data => {
+fullDataset.then(data => {
     console.log(data);    
 
     data.names.forEach(name => {
@@ -20,25 +20,64 @@ d3.json('samples.json').then(data => {
 })
 
 // Populate metadata
-function populateMetadata(selectedSampleID){
-        d3.json('samples.json').then(data => {
+function populateMetadata(sampleID){
+        fullDataset.then(data => {
         
         // Select metadata from complete dataset
         var metadata = data.metadata;
         
         // Filter metadata by sample ID (selectedSample variable) 
-        var sampleData = metadata.filter(sample => sample.id == selectedSampleID);
+        var sampleData = metadata.filter(sample => sample.id ==sampleID);
         var result = sampleData[0];
-        console.log(result)
+        // console.log(result)
         var mdPanel = d3.select("#sample-metadata");
+        // Attention - Address duplicates
+//         mdPanel.property('text', '');
+        
         Object.entries(result).forEach(([key, value]) => {
             mdPanel
+            // .enter()
             .append("h6")
-            .text(`${key}: ${value}`)
-            .property("value",`${key}: ${value}`);
+            .text(`${key}: ${value}`);
         });
     });
 }
 
+//  Top 10 OTUs found in that individual
+function top10(sampleID) {
+    fullDataset.then(data => {
+        
+        // Select metadata from complete dataset
+        var samples = data.samples;
+        // Filter samples by ID
+        var filteredSampleData = samples.filter(sample => sample.id == sampleID);
+        
+        // Pull arrays from the object
+        // var otu_labels = filteredSampleData.map(sample => sample.otu_labels);
+        // var sample_values = filteredSampleData.map(sample => sample.sample_values);
+        
+        // Find top 10 
+        var byOTUValue = filteredSampleData.slice(0);
+        console.log(byOTUValue);
+        byOTUValue.sort(function (a,b) {
+            return b.sample_values - a.sample_values;
+        })
+        // var topOTU = Object.values(filteredSampleData.sample_values).sort(function compareFunction(firstNum, secondNum) {
+        //     return sample_values[secondNum] - sample_values[firstNum]
+        //   });
+        // console.log(filteredSampleData);
+
+        // Object.entries(result).forEach(([key, value]) => {
+});
+}
+
+//  Bubble plot
+
+// Function to update the page when the drop down value is changed
+function optionChanged(newSample) {
+    populateMetadata(newSample);
+    top10(newSample);
+}
+
 // Listener
-sel.on('change', populateMetadata(940));
+//sel.on('change',optionChanged(941));
